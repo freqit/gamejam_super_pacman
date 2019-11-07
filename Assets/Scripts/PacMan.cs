360d40a76cec552db6ed548f71b4a42b0880ed18
freqit <rocktshirts@hotmail.com>
2019-11-07 20:42:29 +0100

    Added basic Pacman movement

---
+++ Assets/Scripts/PacMan.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PacManGame
+{
+    public class PacMan : MonoBehaviour
+    {
+        float Velocity = 6f;
+
+        private Vector2 Direction;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            UserInput();
+
+            Move();
+        }
+
+        public void UserInput()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                Direction = Vector2.left;
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                Direction = Vector2.right;
+            }
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                Direction = Vector2.up;
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Direction = Vector2.down;
+            }
+        }
+
+        public void Move()
+        {
+            transform.Translate(Direction * Velocity * Time.deltaTime);
+        }
+    }
+}
+
