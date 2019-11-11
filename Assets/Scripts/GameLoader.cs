23163b430d9085aaf0e17d7d1198a6819735e24a
pettzon <pettzon91@gmail.com>
2019-11-11 10:20:10 +0100

    Initial GameLoader added
    loads gamemanager and future objects

---
+++ Assets/Scripts/GameLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PacManGame
+{
+    public class GameLoader : MonoBehaviour
+    {
+        public static GameLoader instance;
+        public GameObject gameManager;
+
+        private void Awake()
+        {
+            instance = this;
+
+            if (GameManager.instance == null)
+            {
+                Instantiate(gameManager);
+            }
+        }
+    }
+}
